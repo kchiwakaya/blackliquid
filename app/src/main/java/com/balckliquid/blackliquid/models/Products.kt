44dee6dd2e42a3a826a8c1c3e577a0258aaea917
Kevin Chiwakaya <kchiwakaya@gmail.com>
2024-01-10 10:15:35 +0200

    models finished

---
+++ app/src/main/java/com/balckliquid/blackliquid/models/Products.kt
@@ -0,0 +1,47 @@
+package com.balckliquid.blackliquid.models
+
+import java.sql.Date
+import java.time.temporal.TemporalAmount
+
+data class Product(
+    val name:String,
+    val description:String? = null,
+    val purchase_price: Double,
+    val date_of_purchase:Date,
+    val quantity: Double
+
+)
+data class Sale(
+    val product: Product,
+    val unit_price:  Double,
+    val quantity: Double,
+    val sale_date: Date,
+    val payment_method: String
+)
+
+data class Bill(
+    val bill_name:String,
+    val description: String?= null,
+    val amount: Double,
+    val date_paid: Double
+
+)
+data class Cashout(
+    val amount: Double,
+    val reason:String,
+    val person_name: String,
+    val date_paid: Double
+)
+data class CashIn(
+    val amount: Double,
+    val reason:String,
+    val person_name: String,
+    val date_paid: Double
+)
+data class Loss(
+    val product: Product,
+    val loss_description:String,
+    val unit_price: Double,
+    val quantity: Double,
+    val date_paid: Double
+)
