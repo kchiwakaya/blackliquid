cad3a4eaf5f79036db1cad8cfcee699c311f7273
Kevin Chiwakaya <kchiwakaya@gmail.com>
2024-01-17 12:23:16 +0200

    sweet stuff

--- app/src/main/java/com/balckliquid/blackliquid/ProfitLossFragment.kt
+++ app/src/main/java/com/balckliquid/blackliquid/ProfitLossFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.balckliquid.blackliquid.models.Calculations
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +36,11 @@ class ProfitLossFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profit_loss, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val student = Calculations.expenses()
     }
 
     companion object {
---
+++ app/src/main/java/com/balckliquid/blackliquid/models/Calculations.kt
@@ -0,0 +1,13 @@
+package com.balckliquid.blackliquid.models
+
+class Calculations {
+    companion object{
+
+        fun revenue():Double{
+            return 78.98;
+        }
+        fun expenses():Double{
+            return 0.0
+        }
+    }
+}
